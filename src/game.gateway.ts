e880da59291ff622362cac75502313525c7f93a3
RondoYummy <basrers@ukr.net>
2024-11-02 11:29:05 +0200

    2

--- src/game.gateway.ts
+++ src/game.gateway.ts
@@ -246,6 +246,7 @@ export class GameGateway {
       }
       groupedPlayers[key].push(player);
     });
+    console.log('groupedPlayers: ', groupedPlayers);
 
     for (const key in groupedPlayers) {
       if (groupedPlayers[key].length >= 2) {
